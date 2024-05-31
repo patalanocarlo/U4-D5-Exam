04d2fc6269c05e388ec3a6d46260570f18c9de28
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-31 10:53:58 +0200

    Commento Rivsita e creo una parte iniziale di catalogoelementi

--- src/main/java/Patalanocarlo/Archivio/CatalogoBibliotecario.java
+++ src/main/java/Patalanocarlo/Archivio/CatalogoBibliotecario.java
@@ -1,2 +1,19 @@
 package Patalanocarlo.Archivio;
+import java.util.ArrayList;
+import java.util.List;
 
+
+
+
+public class CatalogoBibliotecario {
+
+    private List<ElementoCatalogo> catalogo;
+
+
+    public CatalogoBibliotecario() {
+        this.catalogo = new ArrayList<>();
+    }
+    public void aggiungiElemento(ElementoCatalogo elemento){
+        catalogo.add(elemento);
+    }
+}
--- src/main/java/Patalanocarlo/Archivio/Rivista.java
+++ src/main/java/Patalanocarlo/Archivio/Rivista.java
@@ -1,16 +1,19 @@
 package Patalanocarlo.Archivio;
-public class Rivista extends ElementoCatalogo {
+public class Rivista extends ElementoCatalogo {  //Estendo a Rivista il suo Padre
 
 
-    public enum Periodicità   {
+    public enum Periodicità   { //creo una sottoclasse periodicità per le settimane e i mesi.
         SETTIMANALE, MENSILE, SEMESTRALE
     }
     private Periodicità periodicità;
 
-    public Rivista(String isbn, String Titolo, int annoPubblicazione, int numeroPagine) {
+    public Rivista(String isbn, String Titolo, int annoPubblicazione, int numeroPagine) { //Mi passo i parametri del padre e faccio il super.
         super(isbn, Titolo, annoPubblicazione, numeroPagine);
         this.periodicità=periodicità;
     }
+
+
+    //Getter e setter del unico attributo di Rivista
     public Periodicità getPeriodicita(){
         return periodicità;
     }
@@ -18,6 +21,8 @@ public class Rivista extends ElementoCatalogo {
     public void setPeriodicita(Periodicità periodicita) {
         this.periodicità = periodicita;
     }
+
+    //ToString finale con il return della Stringa per la rivista.
     public String toString() {
         return "Rivista{" +
                 "periodicita=" + periodicità +
