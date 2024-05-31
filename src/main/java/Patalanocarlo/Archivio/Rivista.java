79114c5865e901a3f31a8999714af93659426329
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-31 10:42:50 +0200

    Elimino classe enum e la faccio tornare  una classe semplice e aggiungo un enum su una sottoclasse

--- src/main/java/Patalanocarlo/Archivio/Rivista.java
+++ src/main/java/Patalanocarlo/Archivio/Rivista.java
@@ -1,4 +1,27 @@
 package Patalanocarlo.Archivio;
+public class Rivista extends ElementoCatalogo {
 
-public enum Rivista {
+
+    public enum Periodicità   {
+        SETTIMANALE, MENSILE, SEMESTRALE
+    }
+    private Periodicità periodicità;
+
+    public Rivista(String isbn, String Titolo, int annoPubblicazione, int numeroPagine) {
+        super(isbn, Titolo, annoPubblicazione, numeroPagine);
+        this.periodicità=periodicità;
+    }
+    public Periodicità getPeriodicita(){
+        return periodicità;
+    }
+
+    public void setPeriodicita(Periodicità periodicita) {
+        this.periodicità = periodicita;
+    }
+    public String toString() {
+        return "Rivista{" +
+                "periodicita=" + periodicità +
+                "} " + super.toString();
+    }
+
 }
