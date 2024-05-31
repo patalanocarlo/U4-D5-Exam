7c27e084135978fa9a38028b76708935042a5c59
patalanocarlo <157119840+patalanocarlo@users.noreply.github.com>
2024-05-31 11:20:35 +0200

    Sono arrivato a impostarmi la ricerca Per Autore al interno del catalogo e aggiungo qualche commento

--- src/main/java/Patalanocarlo/Archivio/CatalogoBibliotecario.java
+++ src/main/java/Patalanocarlo/Archivio/CatalogoBibliotecario.java
@@ -1,8 +1,8 @@
 package Patalanocarlo.Archivio;
 import java.util.ArrayList;
 import java.util.List;
-
-
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 public class CatalogoBibliotecario {
@@ -13,7 +13,26 @@ public class CatalogoBibliotecario {
     public CatalogoBibliotecario() {
         this.catalogo = new ArrayList<>();
     }
+
+    //Metodo per andare ad aggiungere un elemento
     public void aggiungiElemento(ElementoCatalogo elemento){
         catalogo.add(elemento);
+    }
+
+    //Metodo di rimozioni isbn dato il suo stesso isbn
+    public void RimuoviElemento(String isbn){
+        catalogo.removeIf(e -> e.getIsbn().equals(isbn));
+    }
+
+    //Ricerca per Un valore chiave isbn
+    public Optional<ElementoCatalogo> RicercaIsbn(String isbn){
+        return catalogo.stream().filter(e -> e.getIsbn().equals(isbn)).findFirst();
+    }
+
+    //Qui vado a ricercare l'autore in base al filtro e al libro preso vado a fare una get sul autore
+    public  List<ElementoCatalogo> RicercaPerAutore(String autore){
+        return catalogo.stream()
+                .filter(e -> e instanceof Libri && ((Libri) e).getAutore().equalsIgnoreCase(autore))
+                .collect(Collectors.toList());
     }
 }
